9fc8a458fa0b0b4a3b2c5ce1b1ba510534deb96f
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2024-11-22 16:03:16 -0500

    Chore: github action added

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://luisc73.github.io/Sunnyside-landing-page/',
   integrations: [tailwind(), icon()]
 });
